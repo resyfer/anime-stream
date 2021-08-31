10a6f8894ef495ba9e32093c453fd915c2f6acfe
resyfer <resyfer.dev@gmail.com>
2021-08-31 10:36:26 +0530

    Navbar finish]

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 //* React
-import React, { useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 //* Chakra UI
@@ -30,7 +30,7 @@ import './css/Navbar.scss';
 //* Function Component
 const Navbar: React.FC = () => {
 	const { user, loggedIn, search, setSearch } = useContext(UserContext);
-	console.log(search);
+
 	return (
 		<HStack
 			as={'header'}
@@ -41,48 +41,67 @@ const Navbar: React.FC = () => {
 			<Box maxWidth='md' as={Link} to='/'>
 				<Image src='/img/logo.svg' alt='logo' maxHeight='8vh' />
 			</Box>
-			<ButtonGroup>
-				<Button variant='ghost' as={Link} to='/watch'>
-					Watch
-				</Button>
-				<Button variant='ghost' as={Link} to='/list'>
-					List
-				</Button>
-				<Button variant='ghost' as={Link} to='/my-list'>
-					My List
-				</Button>
-			</ButtonGroup>
-			<InputGroup width='sm'>
-				<Input
-					placeholder='Search Anime ...'
-					value={search}
-					width='sm'
-					onChange={e => setSearch(e.target.value)}
-				/>
-				<InputRightAddon as={Link} to='/search'>
-					<SearchIcon />
-				</InputRightAddon>
-			</InputGroup>
+
 			{loggedIn && user && (
-				<Menu>
-					<MenuButton minWidth='5vh' alignItems='center' variant='outline'>
-						{/* <HStack spacing={2}> */}
-						<Image
-							maxHeight='5vh'
-							borderRadius='50%'
-							src={`https://avatars.dicebear.com/api/identicon/${user.name}.svg`}
+				<>
+					<ButtonGroup>
+						<Button variant='ghost' as={Link} to='/watch'>
+							Watch
+						</Button>
+						<Button variant='ghost' as={Link} to='/list'>
+							List
+						</Button>
+						<Button variant='ghost' as={Link} to='/my-list'>
+							My List
+						</Button>
+					</ButtonGroup>
+					<InputGroup width='sm'>
+						<Input
+							placeholder='Search Anime ...'
+							value={search}
+							width='sm'
+							onChange={e => setSearch(e.target.value)}
 						/>
-						{/* </HStack> */}
-					</MenuButton>
-					<MenuList>
-						<MenuItem as={Link} to='/profile'>
-							My Profile
-						</MenuItem>
-						<MenuDivider />
-						//TODO : Add logout
-						<MenuItem color='red'>Logout</MenuItem>
-					</MenuList>
-				</Menu>
+						<InputRightAddon as={Link} to='/search'>
+							<SearchIcon />
+						</InputRightAddon>
+					</InputGroup>
+					<Menu>
+						<MenuButton minWidth='5vh' alignItems='center' variant='outline'>
+							<Image
+								maxHeight='5vh'
+								borderRadius='50%'
+								src={`https://avatars.dicebear.com/api/identicon/${user.name}.svg`}
+							/>
+						</MenuButton>
+						<MenuList>
+							<MenuItem as={Link} to='/profile'>
+								My Profile
+							</MenuItem>
+							<MenuDivider />
+							<MenuItem
+								color='red'
+								onClick={() => {
+									let now = new Date();
+									document.cookie =
+										'jwt= ; expires=' + now.toUTCString() + '; path=/';
+									window.location.reload();
+								}}>
+								Logout
+							</MenuItem>
+						</MenuList>
+					</Menu>
+				</>
+			)}
+			{!loggedIn && (
+				<ButtonGroup>
+					<Button variant='ghost' as={Link} to='/login'>
+						Login
+					</Button>
+					<Button variant='solid' color='blue.500' as={Link} to='/signup'>
+						Signup
+					</Button>
+				</ButtonGroup>
 			)}
 		</HStack>
 	);
